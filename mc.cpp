8031702ffc0570ff0611bad7bb5ec5811e1d175e
Necsulescu Stefan Matei <necsulescu.stefan@gmail.com>
2025-05-24 09:59:04 +0300

    mc.cpp

--- mc.cpp
+++ mc.cpp
@@ -72,6 +72,26 @@ int main()
             fi >> n;
             std::cout << char(n);
         }
+        if(!strcmp(command, "clear"))
+        {
+                system("CLS");
+        }
+        if(!strcmp(command, "rect"))
+        {
+            fi >> x >>y;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                        std::cout << char(219) << char(219);
+                }
+                std::cout << std::endl;
+            }
+        }
+        if(!strcmp(command, ""))
+        {
+
+        }
         if (!strcmp(command, "add"))
         {
             fi >> command2 >> command3;
@@ -100,7 +120,7 @@ int main()
         if (!strcmp(command, "equal"))
         {
             fi >> command2 >> command3;
-            ints[command2] = ints[command3]);
+            ints[command2] = ints[command3];
         }
         if (!strcmp(command, "pow"))
         {
@@ -165,116 +185,136 @@ int main()
                         system(color);
                     }
                     if (!strcmp(command, "int"))
-                    {
-                        fi >> command2 >> c >> n;
-                        ints[command2] = n;
-                    }
-                    if (!strcmp(command, "char"))
-                    {
-                        fi >> command2 >> c >> c1;
-                        chars[command2] = c1;
-                    }
-                    if (!strcmp(command, "string"))
-                    {
-                        fi >> command2 >> c >> command3;
-                        strings[command2] = command3;
-                    }
-                    if (!strcmp(command, "wait"))
-                    {
-                        std::cin >> x;
-                        Sleep(x);
-                    }
-                    if (!strcmp(command, "beep"))
-                    {
-                        fi >> x >> y;
-                        Beep(x, y);
-                    }
-                    if (!strcmp(command, "cls")) system("cls");
-                    if (!strcmp(command, "ctoi"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] = chars[command3];
-                    }
-                    if (!strcmp(command, "itoc"))
-                    {
-                        fi >> command2 >> command3;
-                        chars[command2] = char(ints[command3]);
-                    }
-                    if (!strcmp(command, "cc"))
-                    {
-                        fi >> n;
-                        std::cout << char(n);
-                    }
-                    if (!strcmp(command, "add"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] += ints[command3];
-                    }
-                    if (!strcmp(command, "decrease"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] -= ints[command3];
-                    }
-                    if (!strcmp(command, "multiply"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] *= ints[command3];
-                    }
-                    if (!strcmp(command, "divide"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] /= ints[command3];
-                    }
-                    if (!strcmp(command, "equal"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] = ints[command3]);
-                    }
-                    if (!strcmp(command, "pow"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] = pow(ints[command2], ints[command3]);
-                    }
-                    if (!strcmp(command, "sqrt"))
-                    {
-                        fi >> command2;
-                        ints[command2] = sqrt(ints[command2]);
-                    }
-                    if (!strcmp(command, "mod"))
-                    {
-                        fi >> command2 >> command3;
-                        ints[command2] %= ints[command3];
-                    }
-                    if (!strcmp(command, "write_int"))
-                    {
-                        fi >> command2;
-                        std::cout << ints[command2];
-                    }
-                    if (!strcmp(command, "write_char"))
-                    {
-                        fi >> command2;
-                        std::cout << chars[command2];
-                    }
-                    if (!strcmp(command, "write_string"))
-                    {
-                        fi >> command2;
-                        std::cout << strings[command2];
-                    }
-                    if (!strcmp(command, "read_int"))
-                    {
-                        fi >> command2;
-                        std::cin >> ints[command2];
-                    }
-                    if (!strcmp(command, "read_char"))
-                    {
-                        fi >> command2;
-                        std::cin >> chars[command2];
-                    }
-                    if (!strcmp(command, "read_string"))
-                    {
-                        fi >> command2;
-                        std::cin >> strings[command2];
-                    }
+        {
+            fi >> command2 >> c >> n;
+            ints[command2] = n;
+        }
+        if (!strcmp(command, "char"))
+        {
+            fi >> command2 >> c >> c1;
+            chars[command2] = c1;
+        }
+        if (!strcmp(command, "string"))
+        {
+            fi >> command2 >> c >> command3;
+            strings[command2] = command3;
+        }
+        if (!strcmp(command, "wait"))
+        {
+            std::cin >> x;
+            Sleep(x);
+        }
+        if (!strcmp(command, "beep"))
+        {
+            fi >> x >> y;
+            Beep(x, y);
+        }
+        if (!strcmp(command, "cls")) system("cls");
+        if (!strcmp(command, "ctoi"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] = chars[command3];
+        }
+        if (!strcmp(command, "itoc"))
+        {
+            fi >> command2 >> command3;
+            chars[command2] = char(ints[command3]);
+        }
+        if (!strcmp(command, "cc"))
+        {
+            fi >> n;
+            std::cout << char(n);
+        }
+        if(!strcmp(command, "clear"))
+        {
+                system("CLS");
+        }
+        if(!strcmp(command, "rect"))
+        {
+            fi >> x  >>y;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                        std::cout << char(219) << char(219);
+                }
+                std::cout << std::endl;
+            }
+        }
+        if(!strcmp(command, ""))
+        {
+
+        }
+        if (!strcmp(command, "add"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] += ints[command3];
+        }
+        if (!strcmp(command, "decrease"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] -= ints[command3];
+        }
+        if (!strcmp(command, "multiply"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] *= ints[command3];
+        }
+        if (!strcmp(command, "divide"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] /= ints[command3];
+        }
+        if (!strcmp(command, "mod"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] %= ints[command3];
+        }
+        if (!strcmp(command, "equal"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] = ints[command3];
+        }
+        if (!strcmp(command, "pow"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] = pow(ints[command2], ints[command3]);
+        }
+        if (!strcmp(command, "sqrt"))
+        {
+            fi >> command2;
+            ints[command2] = sqrt(ints[command2]);
+        }
+        if (!strcmp(command, "write_int"))
+        {
+            fi >> command2;
+            std::cout << ints[command2];
+        }
+        if (!strcmp(command, "write_char"))
+        {
+            fi >> command2;
+            std::cout << chars[command2];
+        }
+        if (!strcmp(command, "write_string"))
+        {
+            fi >> command2;
+            std::cout << strings[command2];
+        }
+        if (!strcmp(command, "read_int"))
+        {
+            fi >> command2;
+            std::cin >> ints[command2];
+        }
+        if (!strcmp(command, "read_char"))
+        {
+            fi >> command2;
+            std::cin >> chars[command2];
+        }
+        if (!strcmp(command, "read_string"))
+        {
+            fi >> command2;
+            std::cin >> strings[command2];
+        }
                 }
                 if (!strcmp(command4, "endwhile")) break;
             }
@@ -298,116 +338,136 @@ int main()
                 system(color);
             }
             if (!strcmp(command, "int"))
+        {
+            fi >> command2 >> c >> n;
+            ints[command2] = n;
+        }
+        if (!strcmp(command, "char"))
+        {
+            fi >> command2 >> c >> c1;
+            chars[command2] = c1;
+        }
+        if (!strcmp(command, "string"))
+        {
+            fi >> command2 >> c >> command3;
+            strings[command2] = command3;
+        }
+        if (!strcmp(command, "wait"))
+        {
+            std::cin >> x;
+            Sleep(x);
+        }
+        if (!strcmp(command, "beep"))
+        {
+            fi >> x >> y;
+            Beep(x, y);
+        }
+        if (!strcmp(command, "cls")) system("cls");
+        if (!strcmp(command, "ctoi"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] = chars[command3];
+        }
+        if (!strcmp(command, "itoc"))
+        {
+            fi >> command2 >> command3;
+            chars[command2] = char(ints[command3]);
+        }
+        if (!strcmp(command, "cc"))
+        {
+            fi >> n;
+            std::cout << char(n);
+        }
+        if(!strcmp(command, "clear"))
+        {
+                system("CLS");
+        }
+        if(!strcmp(command, "rect"))
+        {
+            fi >> x >>y;
+            for (int i = 0; i < x; i++)
             {
-                fi >> command2 >> c >> n;
-                ints[command2] = n;
+                for (int j = 0; j < y; j++)
+                {
+                        std::cout << char(219) << char(219);
+                }
+                std::cout << std::endl;
             }
-            if (!strcmp(command, "char"))
-            {
-                fi >> command2 >> c >> c1;
-                chars[command2] = c1;
-            }
-            if (!strcmp(command, "string"))
-            {
-                fi >> command2 >> c >> command3;
-                strings[command2] = command3;
-            }
-            if (!strcmp(command, "wait"))
-            {
-                std::cin >> x;
-                Sleep(x);
-            }
-            if (!strcmp(command, "beep"))
-            {
-                fi >> x >> y;
-                Beep(x, y);
-            }
-            if (!strcmp(command, "cls")) system("cls");
-            if (!strcmp(command, "ctoi"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] = chars[command3];
-            }
-            if (!strcmp(command, "itoc"))
-            {
-                fi >> command2 >> command3;
-                chars[command2] = char(ints[command3]);
-            }
-            if (!strcmp(command, "cc"))
-            {
-                fi >> n;
-                std::cout << char(n);
-            }
-            if (!strcmp(command, "add"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] += ints[command3];
-            }
-            if (!strcmp(command, "decrease"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] -= ints[command3];
-            }
-            if (!strcmp(command, "multiply"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] *= ints[command3];
-            }
-            if (!strcmp(command, "divide"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] /= ints[command3];
-            }
-            if (!strcmp(command, "mod"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] %= ints[command3];
-            }
-            if (!strcmp(command, "equal"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] = ints[command3]);
-            }
-            if (!strcmp(command, "pow"))
-            {
-                fi >> command2 >> command3;
-                ints[command2] = pow(ints[command2], ints[command3]);
-            }
-            if (!strcmp(command, "sqrt"))
-            {
-                fi >> command2;
-                ints[command2] = sqrt(ints[command2]);
-            }
-            if (!strcmp(command, "write_int"))
-            {
-                fi >> command2;
-                std::cout << ints[command2];
-            }
-            if (!strcmp(command, "write_char"))
-            {
-                fi >> command2;
-                std::cout << chars[command2];
-            }
-            if (!strcmp(command, "write_string"))
-            {
-                fi >> command2;
-                std::cout << strings[command2];
-            }
-            if (!strcmp(command, "read_int"))
-            {
-                fi >> command2;
-                std::cin >> ints[command2];
-            }
-            if (!strcmp(command, "read_char"))
-            {
-                fi >> command2;
-                std::cin >> chars[command2];
-            }
-            if (!strcmp(command, "read_string"))
-            {
-                fi >> command2;
-                std::cin >> strings[command2];
-            }
+        }
+        if(!strcmp(command, ""))
+        {
+
+        }
+        if (!strcmp(command, "add"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] += ints[command3];
+        }
+        if (!strcmp(command, "decrease"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] -= ints[command3];
+        }
+        if (!strcmp(command, "multiply"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] *= ints[command3];
+        }
+        if (!strcmp(command, "divide"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] /= ints[command3];
+        }
+        if (!strcmp(command, "mod"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] %= ints[command3];
+        }
+        if (!strcmp(command, "equal"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] = ints[command3];
+        }
+        if (!strcmp(command, "pow"))
+        {
+            fi >> command2 >> command3;
+            ints[command2] = pow(ints[command2], ints[command3]);
+        }
+        if (!strcmp(command, "sqrt"))
+        {
+            fi >> command2;
+            ints[command2] = sqrt(ints[command2]);
+        }
+        if (!strcmp(command, "write_int"))
+        {
+            fi >> command2;
+            std::cout << ints[command2];
+        }
+        if (!strcmp(command, "write_char"))
+        {
+            fi >> command2;
+            std::cout << chars[command2];
+        }
+        if (!strcmp(command, "write_string"))
+        {
+            fi >> command2;
+            std::cout << strings[command2];
+        }
+        if (!strcmp(command, "read_int"))
+        {
+            fi >> command2;
+            std::cin >> ints[command2];
+        }
+        if (!strcmp(command, "read_char"))
+        {
+            fi >> command2;
+            std::cin >> chars[command2];
+        }
+        if (!strcmp(command, "read_string"))
+        {
+            fi >> command2;
+            std::cin >> strings[command2];
+        }
         }
     }
 }
